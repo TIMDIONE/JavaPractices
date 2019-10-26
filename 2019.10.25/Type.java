7fe0f458407241863c918c7baf489fa14fe35ca3
TIMDIONE <1137308584@qq.com>
2019-10-26 20:29:20 +0800

    整形的范围

---
+++ 2019.10.25/Type.java
@@ -0,0 +1,8 @@
+public class Type{
+	public static void main(String[] args){
+		int a = 10;
+		System.out.println(a);
+		long aLong = 0b0110L;
+		System.out.println(aLong);
+		}
+}
---
+++ 2019.10.25/VariableDeom.java
@@ -0,0 +1,26 @@
+public class VariableDeom{
+	public static void main(String[] args){
+		int a;
+		a = 13;
+		System.out.println(a);
+		short minShort = Short.MIN_VALUE;
+		short maxShort = Short.MAX_VALUE;
+		System.out.println(minShort);
+		System.out.println(maxShort);
+		
+		long minLong = Long.MIN_VALUE;
+		long maxLong = Long.MAX_VALUE;
+		System.out.println(minLong);
+		System.out.println(maxLong);
+		
+		byte minByte = Byte.MIN_VALUE;
+		byte maxByte = Byte.MAX_VALUE;
+		System.out.println(minByte);
+		System.out.println(maxByte);
+		
+		int minInt = Integer.MIN_VALUE;
+		int maxInt = Integer.MAX_VALUE;
+		System.out.println(minInt);
+		System.out.println(maxInt);
+	}
+}
